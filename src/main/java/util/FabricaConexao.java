500d83545c049f433227e913d04c4a6eea026183
Larissa <larissaesp02@gmail.com>
2026-05-22 21:57:46 -0300

    Feature: Adiciona Spring Profile

--- src/main/java/util/FabricaConexao.java
+++ src/main/java/util/FabricaConexao.java
@@ -1,19 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
+
+    private static final Properties props = carregarPropriedades();
+
+    private static Properties carregarPropriedades() {
+        Properties p = new Properties();
+        try (InputStream in = FabricaConexao.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (in == null) {
+                throw new RuntimeException("Arquivo db.properties não encontrado no classpath. " +
+                        "Copie db.properties.example para db.properties e configure suas credenciais.");
+            }
+            p.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao carregar db.properties", e);
+        }
+        return p;
+    }
+
     public static Connection getConexaoPostgres() throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
-        String URL = "jdbc:postgresql://localhost:5433/gerenciamento_rh";
-        String USER = "postgres";
-        String PASSWORD = "Nico2005";
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(
+                props.getProperty("db.url"),
+                props.getProperty("db.user"),
+                props.getProperty("db.password")
+        );
     }
 }
